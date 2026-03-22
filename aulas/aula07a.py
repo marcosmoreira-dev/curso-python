6043ee7e631a1053b2f81b8be8981212a22898c7
Marcos Vinícius Moreira da Silva <254775220+marcosmoreira-dev@users.noreply.github.com>
2026-03-22 13:34:50 -0300

    aula de operadores aritméticos e organização dos desafios de 5 a 13

---
+++ aulas/aula07a.py
@@ -0,0 +1,13 @@
+n1 = int(input('Um valor: '))
+n2 = int(input('Outro valor: '))
+soma = n1 + n2
+mult = n1 * n2
+div = n1 / n2
+divInt = n1 // n2
+pot = n1 ** n2
+
+print(f'Soma: {soma}')
+print(f'Multiplicação: {mult}')
+print(f'Divisão: {div}')
+print(f'Divisão Inteira: {divInt}')
+print(f'Potenciação: {pot}')
---
+++ desafios/desafio010.py
@@ -0,0 +1,1 @@
+# Crie um programa que leia o quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar. Considere US$1,00 = R$3,27
