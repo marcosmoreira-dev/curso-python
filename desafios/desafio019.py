3f7ede2aa5ac57e03755591f7014ba9f4fb553d9
Marcos Vinícius Moreira da Silva <254775220+marcosmoreira-dev@users.noreply.github.com>
2026-03-31 22:07:01 -0300

    estudo da aula 8 e organização de exercicios de 16 a 21

---
+++ desafios/desafio019.py
@@ -0,0 +1,1 @@
+# Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome deles e escolhendo o nome escolhido (talvez algo relacionado com random)
